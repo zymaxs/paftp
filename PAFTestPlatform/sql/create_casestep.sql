5c3306e7b849145680e00e0b9ee562f21b8cc33f
Eric <marbury822@hotmail.com>
2014-05-12 14:21:40 +0800

    update sql

--- PAFTestPlatform/sql/create_casestep.sql
+++ PAFTestPlatform/sql/create_casestep.sql
@@ -1,11 +1,11 @@
 
-CREATE TABLE `casecontent` (
+CREATE TABLE `casestep` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
- `content` varchar(2048) DEFAULT NULL,
  `type` varchar(20) DEFAULT NULL,
+ `content` varchar(2048) DEFAULT NULL,
  `testcase_id` int(11) DEFAULT NULL,
   INDEX testcase_ind (testcase_id), 
-  FOREIGN KEY (testcase_id) REFERENCES testcase(id) ON DELETE cascade,
+  FOREIGN KEY (testcase_id) REFERENCES testcase(id) ON DELETE set null,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
