b3f1536fc8d5dd63d7260bdc9aab8870bb2a122d
Vincent-Chou <sicong_zhou@163.com>
2014-05-15 15:27:58 +0800

    Add service Signed-off-by: Vincent-Chou <sicong_zhou@163.com>

---
+++ PAFTestPlatform/src/com/paftp/service/StressResult/StressResultService.java
@@ -0,0 +1,12 @@
+package com.paftp.service.StressResult;
+
+import com.paftp.entity.StressResult;
+
+public interface StressResultService {
+	
+	public void saveStressResult(StressResult stressResult);
+	public void updateStressResult(StressResult stressResult);
+	public StressResult findStressResultById (int id);
+	public StressResult findStressResultBySceneId (int sceneId);
+
+}
