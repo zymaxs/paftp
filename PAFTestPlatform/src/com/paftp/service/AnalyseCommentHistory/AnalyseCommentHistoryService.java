bc944d0d7ac0f625a6ab941ea1617b617dc8faec
Ray <hit-zll@163.com>
2014-05-16 10:52:21 +0800

    service and impl for case comment

---
+++ PAFTestPlatform/src/com/paftp/service/AnalyseCommentHistory/AnalyseCommentHistoryService.java
@@ -0,0 +1,18 @@
+package com.paftp.service.AnalyseCommentHistory;
+
+import java.util.List;
+
+import com.paftp.entity.AnalyseCommentHistory;
+
+public interface AnalyseCommentHistoryService {
+	
+	public void saveAnalyseCommentHistory(AnalyseCommentHistory analyseCommentHistory);
+
+	public void updateAnalyseCommentHistory(AnalyseCommentHistory analyseCommentHistory);
+
+	public AnalyseCommentHistory findAnalyseCommentHistoryById(int id);
+
+	public void deleteAnalyseCommentHistory(AnalyseCommentHistory analyseCommentHistory);
+
+	public List<AnalyseCommentHistory> findAllList();
+}
---
+++ PAFTestPlatform/src/com/paftp/service/AnalyseCommentHistory/impl/AnalyseCommentHistoryServiceImpl.java
@@ -0,0 +1,53 @@
+package com.paftp.service.AnalyseCommentHistory.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.paftp.dao.BaseDAO;
+import com.paftp.entity.AnalyseCommentHistory;
+import com.paftp.service.AnalyseCommentHistory.AnalyseCommentHistoryService;
+
+@Service("analysecommenthistoryService")
+public class AnalyseCommentHistoryServiceImpl implements AnalyseCommentHistoryService{
+
+
+	@Resource
+	private BaseDAO<AnalyseCommentHistory> baseDAO;
+	
+	@Override
+	public void saveAnalyseCommentHistory(
+			AnalyseCommentHistory analyseCommentHistory) {
+		// TODO Auto-generated method stub
+		baseDAO.save(analyseCommentHistory);
+	}
+
+	@Override
+	public void updateAnalyseCommentHistory(
+			AnalyseCommentHistory analyseCommentHistory) {
+		// TODO Auto-generated method stub
+		baseDAO.update(analyseCommentHistory);
+	}
+
+	@Override
+	public AnalyseCommentHistory findAnalyseCommentHistoryById(int id) {
+		// TODO Auto-generated method stub
+		return baseDAO.get(AnalyseCommentHistory.class, id);
+	}
+
+	@Override
+	public void deleteAnalyseCommentHistory(
+			AnalyseCommentHistory analyseCommentHistory) {
+		// TODO Auto-generated method stub
+		baseDAO.delete(analyseCommentHistory);
+	}
+
+	@Override
+	public List<AnalyseCommentHistory> findAllList() {
+		// TODO Auto-generated method stub
+		return baseDAO.find(" from analysecommenthistory u order by u.id");
+	}
+
+}
