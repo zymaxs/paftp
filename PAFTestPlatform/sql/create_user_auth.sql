1a0b6dca2f763f0fd794e1358b20c445ef42e795
EricDuane <duan.juding@gmail.com>
2014-05-17 13:47:32 +0800

    delele sql/create_auth.sql

--- PAFTestPlatform/sql/create_user_auth.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user_auth` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `user_id` int(11) NOT NULL,
- `auth_id` int(11) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
