84b705eb95503491cde4edac49b43532207d222d
Eric <marbury822@hotmail.com>
2014-05-21 15:46:45 +0800

    update testsuite

--- PAFTestPlatform/sql/create_testsuite.sql
+++ PAFTestPlatform/sql/create_testsuite.sql
@@ -1,11 +1,14 @@
 
 CREATE TABLE `testsuite` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `name` varchar(50) DEFAULT NULL,
- `sut_id` int(11) DEFAULT NULL,
-  INDEX sut_ind (sut_id), 
-  FOREIGN KEY (sut_id) REFERENCES sut(id) ON DELETE cascade,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) DEFAULT NULL,
+    `code` varchar(50) DEFAULT NULL,
+    `sut_id` int(11) DEFAULT NULL,
+    INDEX sut_ind (sut_id),
+    FOREIGN KEY (sut_id)
+        REFERENCES sut (id)
+        ON DELETE cascade,
+    PRIMARY KEY (`id`)
+)  ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 
