ff51d671842ac7fe86b1acd4fcbd84fbd1f77c88
Ray <hit-zll@163.com>
2014-05-22 18:36:45 +0800

    RoleAction.java

---
+++ PAFTestPlatform/src/com/paftp/action/RoleAction.java
@@ -0,0 +1,11 @@
+package com.paftp.action;
+
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller
+public class RoleAction extends ActionSupport{
+	
+	
+}
