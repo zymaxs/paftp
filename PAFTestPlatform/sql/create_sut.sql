07a8ec77b96c540d6b643be2f05d56effea2691c
Ray <hit-zll@163.com>
2014-05-23 23:07:55 +0800

    sut group_id

--- PAFTestPlatform/sql/create_sut.sql
+++ PAFTestPlatform/sql/create_sut.sql
@@ -4,6 +4,7 @@ CREATE TABLE `sut` (
     `code` varchar(20) not NULL,
     `name` varchar(100) not NULL UNIQUE,
     `description` varchar(200) DEFAULT NULL,
+    `group_id` int(11) NOT NULL,
     PRIMARY KEY (`id`)
 )  ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
