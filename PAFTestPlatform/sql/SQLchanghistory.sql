d735fd53f0e9a57454b3fb1475659ec6f8a1892b
Ray <hit-zll@163.com>
2014-05-25 00:41:15 +0800

    SQLchanghistory.sql

--- PAFTestPlatform/sql/SQLchanghistory.sql
+++ PAFTestPlatform/sql/SQLchanghistory.sql
@@ -6,4 +6,7 @@ ADD UNIQUE INDEX `code_UNIQUE` (`code` ASC);
 ALTER TABLE `paftp`.`sut` CHANGE COLUMN `group_id` `group_id` INT(11) NULL DEFAULT NULL ;
 ALTER TABLE `paftp`.`sut` ADD FOREIGN KEY (`group_id`) REFERENCES `sutgroup` (`id`) ON DELETE set null;
 
+2014/5/25  00:34
+CREATE SCHEMA `paftp` DEFAULT CHARACTER SET utf8 ;
+
 
