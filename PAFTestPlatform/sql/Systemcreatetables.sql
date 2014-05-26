16ededdb58d16cc803f6fdbde1606fe163c76d4f
Ray <hit-zll@163.com>
2014-05-26 13:49:48 +0800

    change for bat sql

---
+++ PAFTestPlatform/sql/Systemcreatetables.sql
@@ -0,0 +1,36 @@
+DROP DATABASE `paftp`;
+create SCHEMA `paftp` DEFAULT CHARACTER SET utf8;
+USE `paftp`;
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_version.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_userinfo_position.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_userinfo_department.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_userinfo.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_user.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_sut_group.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_sut.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_role.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_user_role.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_scene.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_strategetype.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_stressresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_tpsresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_testsuite.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_testcase.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_permission.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_role_permission.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_responsetimeresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_networkresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_memoryresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_errorlog.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_diskresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_cpuresult.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_casestep.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_casechangehistory.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_casechangeoperation.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_applysut.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_testpass.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_testsuite_result.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_testcase_result.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_testcase_result_content.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_analysecomment_history.sql
+source C:\GitHub\paftp\PAFTestPlatform\sql\create_analysecomment_content.sql
---
+++ PAFTestPlatform/sql/resetdatabase.bat
@@ -0,0 +1,10 @@
+@echo off
+C:
+cd C:\Program Files\MySQL\MySQL Server 5.6\bin
+@set db=paftp
+@set userName=root
+@set password=Paic#234
+@set sqlpath=C:\GitHub\paftp\PAFTestPlatform\sql\Systemcreatetables.sql
+mysql -f -u %userName% --password=%password% %db% < %sqlpath% --default-character-set=utf8
+@echo "Finish to re-create the database and tables!"
+pause
