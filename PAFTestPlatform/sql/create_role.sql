f92afdf37486800a000f90c6525d6ab084ce277b
Ray <hit-zll@163.com>
2014-06-19 17:59:46 +0800

    sql

--- PAFTestPlatform/sql/create_role.sql
+++ PAFTestPlatform/sql/create_role.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE `role` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(20) not NULL,
+    `name` varchar(20) not NULL UNIQUE,
     `description` varchar(30) DEFAULT NULL,
     `sut_id` integer(11) DEFAULT NULL,
       INDEX sut_ind (sut_id), 
