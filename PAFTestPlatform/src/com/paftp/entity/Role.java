7ef24b0c6f0c5f585a76f87db3f182099f8dfd9a
Ray <hit-zll@163.com>
2014-07-10 14:08:53 +0800

    Role

--- PAFTestPlatform/src/com/paftp/entity/Role.java
+++ PAFTestPlatform/src/com/paftp/entity/Role.java
@@ -56,7 +56,7 @@ public class Role {
 		this.description = description;
 	}
 	
-	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
 	@JoinTable(name = "role_permission", joinColumns = { @JoinColumn(name = "role_id") }, inverseJoinColumns = { @JoinColumn(name = "permission_id") })
 	public List<Permission> getPermissions() {
 		return permissions;
