76e443f8897e8083489f2717e9469b199b438fa1
Ray <hit-zll@163.com>
2014-08-25 16:12:43 +0800

    TestcaseResultDto.java

---
+++ PAFTestPlatform/src/com/paftp/dto/TestcaseResultDto.java
@@ -0,0 +1,33 @@
+package com.paftp.dto;
+
+public class TestcaseResultDto {
+
+	private Integer id;
+	private Boolean ispass;
+	private String description;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Boolean getIspass() {
+		return ispass;
+	}
+
+	public void setIspass(Boolean ispass) {
+		this.ispass = ispass;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
